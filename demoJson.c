be6b2bd967951e75785f86cc9f5888f0547100ec
cxy <2830206832@qq.com>
2024-01-23 11:42:54 +0800

    json测试

--- demoJson.c
+++ demoJson.c
@@ -27,66 +27,66 @@ int main(int argc, char *argv[])
     printf("%s\n", str2);
     // json_object_put(jobj);
     json_object * jobj2 = json_tokener_parse(str2);
+#if 0
+    // /* 打印 json中的name */
+    // json_object *jname = json_object_object_get(jobj, "name");
+    // const char *namestr = json_object_get_string(jname);
+    // printf("%s\n", namestr);
 
-    // // /* 打印 json中的name */
-    // // json_object *jname = json_object_object_get(jobj, "name");
-    // // const char *namestr = json_object_get_string(jname);
-    // // printf("%s\n", namestr);
-
-    // // /* 判断 json中是否有phonenum */
-    // // json_object *jphone = json_object_object_get(jobj, "phonenum");
-    // // if (jphone == NULL)
-    // // {
-    // //     printf("no phonenum\n");
-    // // }
-
-    // // /* 修改json中的name */
-    // // json_object_object_add(jobj, "name", json_object_new_string("lisi"));
-
-
-    // // /* 删除json中的name */
-    // // json_object_object_del(jobj, "name");
-    // // namestr = json_object_get_string(jname);
-    // // if (namestr == NULL)
-    // // {
-    // //     printf("name is NULL\n");
-    // // }
-
-    // // /* 修改json中的age */
-    // // json_object_object_add(jobj, "age", json_object_new_int(20));
-    // // int age2 = json_object_get_int(jphone);
-    // // printf("%d\n", age2);
-
-    // // /* 获取json的长度 */
-    // // int len = json_object_object_length(jobj);
-    // // printf("jsonlen = %d\n", len);
-    
-    // /* 遍历json */
-    // json_object_object_foreach(jobj, key, val)
+    // /* 判断 json中是否有phonenum */
+    // json_object *jphone = json_object_object_get(jobj, "phonenum");
+    // if (jphone == NULL)
     // {
-    //     printf("key = %s, val = %s\n", key, json_object_get_string(val));
-    //     /* 
-    //     // other
-    //     if (strcmp(key, "name") == 0)
-    //     {
-    //         printf("name = %s\n", json_object_get_string(val));
-    //         strcpy(name, json_object_get_string(val));
-    //         printf("name = %s\n", name);
-    //     }
-    //     if (strcmp(key, "age") == 0)
-    //     {
-    //         printf("age = %d\n", json_object_get_int(val));
-    //         age = json_object_get_int(val);
-    //         printf("age = %d\n", age);
-    //     }
-    //     if (strcmp(key, "adder") == 0)
-    //     {
-    //         printf("adder = %s\n", json_object_get_string(val));
-    //         strcpy(adder, json_object_get_string(val));
-    //         printf("adder = %s\n", adder);
-    //     }
-    //     */ 
+    //     printf("no phonenum\n");
     // }
+
+    // /* 修改json中的name */
+    // json_object_object_add(jobj, "name", json_object_new_string("lisi"));
+
+
+    // /* 删除json中的name */
+    // json_object_object_del(jobj, "name");
+    // namestr = json_object_get_string(jname);
+    // if (namestr == NULL)
+    // {
+    //     printf("name is NULL\n");
+    // }
+
+    // /* 修改json中的age */
+    // json_object_object_add(jobj, "age", json_object_new_int(20));
+    // int age2 = json_object_get_int(jphone);
+    // printf("%d\n", age2);
+
+    // /* 获取json的长度 */
+    // int len = json_object_object_length(jobj);
+    // printf("jsonlen = %d\n", len);
+#endif 
+    /* 遍历json */
+    json_object_object_foreach(jobj, key, val)
+    {
+        printf("key = %s, val = %s\n", key, json_object_get_string(val));
+        #if 0
+        // other
+        if (strcmp(key, "name") == 0)
+        {
+            printf("name = %s\n", json_object_get_string(val));
+            strcpy(name, json_object_get_string(val));
+            printf("name = %s\n", name);
+        }
+        if (strcmp(key, "age") == 0)
+        {
+            printf("age = %d\n", json_object_get_int(val));
+            age = json_object_get_int(val);
+            printf("age = %d\n", age);
+        }
+        if (strcmp(key, "adder") == 0)
+        {
+            printf("adder = %s\n", json_object_get_string(val));
+            strcpy(adder, json_object_get_string(val));
+            printf("adder = %s\n", adder);
+        }
+        #endif
+    }
 
     /* 创建数组类型的json */
     json_object * jarr = json_object_new_array();
